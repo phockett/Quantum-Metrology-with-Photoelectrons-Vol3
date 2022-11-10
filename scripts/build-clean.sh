eb2ad36f3fab2211421c2dbec08e6ba51b0fc342
phockett <phockett@users.noreply.github.com>
2022-11-10 15:59:18 -0500

    Added basic local build scripts.

---
+++ scripts/build-clean.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Quick local build for QM3 book from source
+#
+# For more options see https://jupyterbook.org/en/stable/basics/build.html
+#
+
+# Set default path, or pass at CLI
+# BASEPATH="${1:-~/QM3}"
+BASEPATH="${1:-/home/jovyan/QM3}"
+
+cd $BASEPATH
+
+echo Running **CLEAN** Jupyter book builds from $BASEPATH
+
+# Clean - should set as option
+jupyter-book clean doc-source/
+
+# Build HTML
+jupyter-book build doc-source/
+
+# Build PDF
+# jupyter-book build doc-source/ --builder pdflatex
+
+# As above, but skip errors
+jupyter-book build --keep-going doc-source/ --builder pdflatex
+
+# Debug for missing refs. as Warnings - note this should also keep going, but doesn't and may get no PDF
+# See https://jupyterbook.org/en/stable/basics/build.html#debug-your-books-build-process
+# jupyter-book build -W -n --keep-going doc-source/ --builder pdflatex
---
+++ scripts/build-ipynb.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Quick local build for QM3 book from source
+#
+# For more options see https://jupyterbook.org/en/stable/basics/build.html
+#
+
+# Set default path, or pass at CLI
+# BASEPATH="${1:-~/QM3}"
+BASEPATH="${1:-/home/jovyan/QM3}"
+BUILDSOURCE="_ipynb_build"
+
+cd $BASEPATH
+
+echo Running Jupyter book builds from $BASEPATH - IPYNB ONLY VERSION
+
+echo Copying ipynb source files from doc-source to $BUILDSOURCE
+mkdir -p $BUILDSOURCE
+# COPY only required files - easy to miss stuff here
+# cp doc-source/* --parents $BUILDSOURCE
+# cp doc-source/**/*.ipynb --parents $BUILDSOURCE
+# COPY full dir, then remove .md
+cp doc-source/ -r $BUILDSOURCE
+echo Removing .md from subdirs using $BUILDSOURCE/doc-source/**/*.md 
+rm $BUILDSOURCE/doc-source/**/*.md
+
+# Clean - should set as option
+# jupyter-book clean doc-source/
+
+# Build HTML
+jupyter-book build $BUILDSOURCE/doc-source/
+
+# Build PDF
+# jupyter-book build doc-source/ --builder pdflatex
+
+# As above, but skip errors
+# jupyter-book build --keep-going doc-source/ --builder pdflatex
+
+# Debug for missing refs. as Warnings - note this should also keep going, but doesn't and may get no PDF
+# See https://jupyterbook.org/en/stable/basics/build.html#debug-your-books-build-process
+# jupyter-book build -W -n --keep-going doc-source/ --builder pdflatex
